c38ca61dd45699925c4eded9e3cee9bcdc4e09ac
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 09:51:48 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan1biodata/IF1101108015Latihan1biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan1biodata;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program Untuk Menampilkan Biodata
+ */
+public class IF1101108015Latihan1biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------------------------------");
+	System.out.println("NAMA\t\t: Adi Chandra Nugraha");
+	System.out.println("ALAMAT\t\t: Cicalengka");
+	System.out.println("TEMPAT LAHIR\t: Bandung");
+	System.out.println("TANGGAL LAHIR\t: 19 September 2000");
+	System.out.println("---------------------------------------------");
+	System.out.println("Hobi\t\t: Bermain Game");
+	System.out.println("MAKES\t\t: BALA-BALA");
+	System.out.println("MIKES\t\t: Thai Tea");
+    }
+    
+}
